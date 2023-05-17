8801e0f560c31fa3780bb758e0e9fe5a08c480eb
nikolastanojevic16 <48658075+nikolastanojevic16@users.noreply.github.com>
2023-05-17 23:06:43 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -2,26 +2,36 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-MAIN()
-{
-echo -e " \nEnter your username:"
+echo "Enter your username:"
 read USERNAME
 USER_ID=$($PSQL "select user_id from users where username='$USERNAME'")
 
 #if user does not exists
 if [[ -z $USER_ID ]]
 then
-  INSERT_USER
+  if [[ ${#USERNAME} -le 22 ]]
+  then
+    # insert major
+    INSERT_USER=$($PSQL "insert into users(username) values('$USERNAME')")
+    if [[ $INSERT_USER == "INSERT 0 1" ]]
+    then
+      echo "Welcome, $USERNAME! It looks like this is your first time here."
+      #get user id
+      USER_ID=$($PSQL "select user_id from users where username='$USERNAME'")
+    fi
+  else
+  echo "Your username must be less or equal to 22 characters"
+  fi
 else
 COUNT_GAMES=$($PSQL "select count(*) from games where user_id='$USER_ID'")
 BEST_SCORE=$($PSQL "select min(number_of_guesses) from games where user_id='$USER_ID'")
-echo -e "Welcome back, $USERNAME! You have played $COUNT_GAMES games, and your best game took $BEST_SCORE guesses."
+echo "Welcome back, $USERNAME! You have played $COUNT_GAMES games, and your best game took $BEST_SCORE guesses."
 fi
 
 CORRECT_NUMBER=$(($RANDOM % 1000 + 1))
 NUMBER_OF_GUESSES=0
 
-echo -e "Guess the secret number between 1 and 1000:"
+echo "Guess the secret number between 1 and 1000:"
 
 read USER_GUESS
 
@@ -59,25 +69,4 @@ if [[ "$USER_GUESS" -eq "$CORRECT_NUMBER" ]]
     then
       echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $CORRECT_NUMBER. Nice job!"
     fi
-  fi
-
-}
-
-INSERT_USER()
-{
-  if [[ ${#USERNAME} -le 22 ]]
-  then
-    # insert major
-    INSERT_USER=$($PSQL "insert into users(username) values('$USERNAME')")
-    if [[ $INSERT_USER == "INSERT 0 1" ]]
-    then
-      echo "Welcome, $USERNAME! It looks like this is your first time here."
-      #get user id
-      USER_ID=$($PSQL "select user_id from users where username='$USERNAME'")
-    fi
-  else
-  echo -e "Your username must be less or equal to 22 characters"
-  fi
-}
-
-MAIN
+fi
